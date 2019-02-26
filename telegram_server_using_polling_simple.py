06ac72b8fda9a890326d83ffa460e5e1e503289f
Chinseok Lee <me@askcompany.kr>
2019-02-26 17:02:47 +0900

    added Python 4.ipynb

--- telegram_server_using_polling_simple.py
+++ telegram_server_using_polling_simple.py
@@ -4,36 +4,11 @@
 
 
 import os
-import re
 import sys
 import traceback
 from telegram.ext import Updater, Filters
 from telegram.ext import CommandHandler, ConversationHandler, MessageHandler
-import requests
-from bs4 import BeautifulSoup
-
-
-def naver_blog_search(keyword):
-    search_url = "https://search.naver.com/search.naver"
-    params = {
-        'where': 'post',
-        'query': keyword,
-    }
-
-    res = requests.get(search_url, params=params)
-    html = res.text
-    soup = BeautifulSoup(html, 'html.parser')
-
-    tag_list = soup.select('.sh_blog_title')
-
-    for tag in tag_list:
-        post_url = tag['href']
-        post_title = tag['title']
-        post = {
-            'title': post_title,
-            'url': post_url,
-        }
-        yield post
+import tasks
 
 
 def start(bot, update):
@@ -46,18 +21,18 @@ def echo(bot, update):
     chat_id = update.message.chat_id  # 대화방 ID
     text = update.message.text
 
+    task_cls_list = [
+        # tasks.YaTask,
+        tasks.HelloTask,
+        tasks.NaverBlogSearchTask,
+    ]
+
     try:
-        matched = re.match(r"네이버에?서?\s*(.+)\s*검색", text)
-        if matched:
-            keyword = matched.groups()[0]
-            line_list = []
-            for post in naver_blog_search(keyword):
-                # line = "{}\n{}".format(post['title'], post['url'])
-                line = "{title}\n{url}".format(**post)
-                line_list.append(line)
-            response = '\n\n'.join(line_list)
-        elif text == '야':
-            response = '왜?'
+        for task_cls in task_cls_list:
+            task = task_cls(text)
+            if task.is_valid():
+                response = task.proc()
+                break
         else:
             response = '니가 무슨 말 하는 지 모르겠어. :('
     except Exception as e:
@@ -85,7 +60,7 @@ def main(token):
 if __name__ == '__main__':
     # TODO: 필요한 라이브러리 설치 : pip install python-telegram-bot requests beautifulsoup4
     # FIXME: 각자의 Token을 적용해주세요.
-    TOKEN = os.environ.get('TELEGRAM_TOKEN')
+    TOKEN = "745214408:AAGPGmS8wlcWE_3-B0rhx0BMkcoI5Y52qwg"
     if TOKEN is None:
         print('ERROR) TELEGRAM_TOKEN을 지정해주세요.', file=sys.stderr)
         sys.exit(1)
